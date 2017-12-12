559d8e21fb73b1d23bcaeeb24c635b643aafaf4c
JSpeed8th <jspeed8th@gmail.com>
2017-12-12 03:53:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName() {
-  var customerName = customerName.toUpperCase();
+  var customerName.toUpperCase();
 }
 
 function setBestCustomer() {
