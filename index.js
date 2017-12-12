a7e3d6787995bba6efaa84ae1d6c2d1f80938dc6
JSpeed8th <jspeed8th@gmail.com>
2017-12-12 03:48:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function overwriteBestCustomer() {
 const leastFavoriteCustomer = 'Jordan';
 
 function changeLeastFavoriteCustomer() {
-  let leastFavoriteCustomer = 'Brittany';
 }
 
 function attemptTwoFavoriteCustomers() {
